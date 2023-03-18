6a42010bf8cdae4d1f02de49fa6772523394450b
PRAKAMYA SHRIVAS <126973768+Prakamya2410@users.noreply.github.com>
2023-03-18 18:03:14 +0530

    Add files via upload

---
+++ SpriteStacking/settings.py
@@ -0,0 +1,83 @@
+import pygame as pg
+
+vec2 = pg.math.Vector2
+
+RES = WIDTH, HEIGHT = vec2(1600, 900)
+CENTER = H_WIDTH, H_HEIGHT = RES // 2
+TILE_SIZE = 250
+
+PLAYER_SPEED = 0.4
+PLAYER_ROT_SPEED = 0.0015
+
+BG_COLOR = 'olivedrab'
+NUM_ANGLES = 72  # multiple of 360 -> 24, 30, 36, 40, 45, 60, 72, 90, 120, 180
+
+# entity sprite settings
+ENTITY_SPRITE_ATTRS = {
+    'player': {
+        'path': 'assets/entities/player/player.png',
+        'num_layers': 7,
+        'scale': 0.35,
+        'y_offset': 0,
+    },
+    'kitty': {
+        'path': 'assets/entities/cats/kitty.png',
+        'num_layers': 8,
+        'scale': 0.8,
+        'y_offset': -20,
+    },
+}
+
+# stacked sprites settings
+STACKED_SPRITE_ATTRS = {
+    'grass': {
+        'path': 'assets/stacked_sprites/grass.png',
+        'num_layers': 11,
+        'scale': 7,
+        'y_offset': 20,
+        'outline': False,
+    },
+    'blue_tree': {
+        'path': 'assets/stacked_sprites/blue_tree.png',
+        'num_layers': 43,
+        'scale': 8,
+        'y_offset': -130,
+    },
+    'car': {
+        'path': 'assets/stacked_sprites/car.png',
+        'num_layers': 9,
+        'scale': 10,
+        'y_offset': 10,
+    },
+    'van': {
+        'path': 'assets/stacked_sprites/van.png',
+        'num_layers': 20,
+        'scale': 6,
+        'y_offset': 10,
+    },
+    'tank': {
+        'path': 'assets/stacked_sprites/tank.png',
+        'num_layers': 17,
+        'scale': 8,
+        'y_offset': 0,
+    },
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
